5c663c0ca79bfb3f37db2ad804cb3893c6bbb572
Robin Turon-Lagot <robin.turon-lagot@etu.univlyon1.fr>
2017-02-03 12:05:22 +0100

    test

--- src/Lines/Controller/IndexController.php
+++ src/Lines/Controller/IndexController.php
@@ -11,14 +11,13 @@ class IndexController
     public function listAll(Request $request, Application $app)
     {
         $linesData = $app['repository.line']->getAll();
-
+$lineEntityList =array();
         foreach ($linesData as $lineData) {
             
             $lineEntityList[$lineData['id']] = (new Line($lineData['id'], $lineData['number'], $lineData['terminuses'], $lineData['way']))->toArray();
             
         }
 
-        
         return json_encode($lineEntityList);
     }
 }
